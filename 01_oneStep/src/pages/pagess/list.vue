d2d16cdc10cf081ad4314b01f5cfccb015a35ed3
启彤 栾 <wanqqq29@gmail.com>
2021-08-05 09:44:56 +0800

    todo APi加密

--- 01_oneStep/src/pages/pagess/list.vue
+++ 01_oneStep/src/pages/pagess/list.vue
@@ -1,70 +1,83 @@
 <template>
-  <div class="q-pa-sm">
-    <q-splitter
-      v-model="splitterModel"
-      disable
-    >
+  <q-page>
+    <div class="q-pa-sm">
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-3">
+              <q-tabs
+                v-model="tab"
+                vertical
+                class="text-teal"
+              >
+                <q-tab
+                  v-for="(i,index) in tabpanel.data"
+                  :name=i.ckID
+                  :key="index"
+                  :label=i.label
+                />
+              </q-tabs>
 
-      <template v-slot:before>
-        <q-page-sticky position="top-left">
-          <q-tabs
-            v-model="tab"
-            vertical
-            class="text-teal"
-          >
-            <q-tab
-              v-for="(i,index) in tabpanel.data"
-            :name=i.ckID
-            :key="index"
-            :label=i.label
-            />
-          </q-tabs>
-        </q-page-sticky>
-      </template>
+            </div>
 
-      <template v-slot:after>
-        <q-tab-panels
-          v-model="tab"
-          animated
-          vertical
-          transition-prev="jump-up"
-          transition-next="jump-up"
-        >
-          <q-tab-panel
-            v-for="(i,index) in tabpanel.data"
-            :name=i.ckID
-            :key="index"
-            :label=i.label
-          >
-            <q-card>
-              <q-img :src=i.mSrc>
-                <div class="text-h5 absolute-bottom text-right">
-                  {{i.label}}
-                </div>
-              </q-img>
-              <q-list>
-                <q-item
-                  clickable
-                  v-for="ii in card[i.ckID]"
-                  :key="ii"
+            <div class="col-9">
+              <q-tab-panels
+                v-model="tab"
+                animated
+                vertical
+                transition-prev="jump-up"
+                transition-next="jump-up"
+              >
+                <q-tab-panel
+                  v-for="(i,index) in tabpanel.data"
+                  :name=i.ckID
+                  :key="index"
+                  :label=i.label
                 >
-                  <q-item-section>
-                    <q-img :src=ii.imgSrc />
-                  </q-item-section>
+                  <q-card>
+                    <q-img :src=i.mSrc>
+                      <div class="text-h5 absolute-bottom text-right">
+                        {{i.label}}
+                      </div>
+                    </q-img>
+                    <q-list>
+                      <q-item
+                        clickable
+                        v-for="ii in card[i.ckID]"
+                        :key="ii"
+                      >
+                        <q-item-section>
+                          <q-img :src=ii.imgSrc />
+                        </q-item-section>
 
-                  <q-item-section>
-                    <q-item-label hreader>{{ii.sName}}</q-item-label>
-                    <q-item-label overline>￥{{ii.sPrice}}</q-item-label>
-                    <q-item-label caption>{{ii.sDesc}}</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-card>
-          </q-tab-panel>
-        </q-tab-panels>
-      </template>
-    </q-splitter>
-  </div>
+                        <q-item-section>
+                          <q-item-label hreader>{{ii.sName}}</q-item-label>
+                          <q-item-label overline>￥{{ii.sPrice}}</q-item-label>
+                          <q-item-label caption>{{ii.sDesc}}</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-card>
+                </q-tab-panel>
+              </q-tab-panels>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="10"
+      :offset="[18, 18]"
+    >
+      <q-btn
+        fab
+        icon="keyboard_arrow_up"
+        color="primary"
+      />
+    </q-page-scroller>
+  </q-page>
 </template>
 
 <script>
@@ -74,9 +87,9 @@ import { api } from 'boot/axios';
 
 
 export default {
-  setup () {
+  setup() {
     const tabpanel = reactive({
-      data:''
+      data: ''
     })
     const card = ref([{
       imgSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
@@ -102,19 +115,19 @@ export default {
     //       console.log(error)
     //     })
     // }
- function load (a) {
+    function load(a) {
       api.get('/tabpanelApi/?pos=' + a)
         .then((response) => {
           console.log(response['data']['tab'])
           tabpanel.data = response['data']['tab']
           tab.value = response['data']['tab'][0]['ckID']
-          
+
         })
         .catch(error => {
           console.log(error)
         })
 
-        api.get('/cardApi/')
+      api.get('/cardApi/')
         .then((response) => {
           card.value = response['data']
           console.log(response['data'])
@@ -127,12 +140,12 @@ export default {
 
     onMounted(() => {
       load(route.params.pos)
-      console.log(route.params.pos,'pos')
-      console.log(route.params.loc,'loc')
+      console.log(route.params.pos, 'pos')
+      console.log(route.params.loc, 'loc')
     })
 
     return {
-      splitterModel: 30,
+      splitterModel: 20,
       tabpanel,
       card,
       load,
